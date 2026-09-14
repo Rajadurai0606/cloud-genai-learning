ba15ea2207dc50884ca6e96e0017e995caeb5fba
Raja <Rajadurai.Kalyanasundaram@lifex.cloud>
2026-09-14 21:16:58 +0100

    Day 2 - add employee request validation

--- 01-python-fastapi/app/main.py
+++ 01-python-fastapi/app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI(
     title="New Learning API",
 )
+
+class Employee(BaseModel):
+    id: str = Field(description="Employee Id")
+    name: str = Field(min_length=3, description="Employee Name")
+    age: int = Field(ge=18, description="Employee Age")
+    department: str | None = Field(default=None)
 
 @app.get("/")
 def root():
@@ -14,3 +21,14 @@ def health_check():
         "status": "healthy",
         "service": "new-learning-api"
     }
+
+@app.post("/employees")
+def add_employee(employee: Employee):
+    print("will be implementing to connect with db and add employee details later")
+    # convert into dictionary
+    print(employee.model_dump())
+    # converts into json foramt
+    print(employee.model_dump_json())
+    # to view schema
+    print(employee.model_json_schema())
+    return employee
