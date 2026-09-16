189b7c9dddfdf29e9d9506cdf6cf588aa9f2e94f
Raja <Rajadurai.Kalyanasundaram@lifex.cloud>
2026-09-16 20:42:56 +0100

    Day 4 - complete employee CRUD API

--- 01-python-fastapi/app/main.py
+++ 01-python-fastapi/app/main.py
@@ -5,16 +5,29 @@ app = FastAPI(
     title="New Learning API",
 )
 
-class Employee(BaseModel):
-    id: str = Field(description="Employee Id")
+"""
+should the client also send: id for update?
+introduced EmployeeBase which will not expose id for
+    adding employee
+    updating employee
+"""
+
+class EmployeeBase(BaseModel):
     name: str = Field(min_length=3, description="Employee Name")
     age: int = Field(ge=18, description="Employee Age")
     department: str | None = Field(default=None)
 
+class EmployeeModel(EmployeeBase):
+    id: int = Field(description="Employee Id")
+
+## API Endpoints
+
+## root
 @app.get("/")
 def root():
     return "hello Raja"
 
+### Health
 @app.get("/health")
 def health_check():
     return{
@@ -22,32 +35,73 @@ def health_check():
         "service": "new-learning-api"
     }
 
-employees: list[Employee] = []
+emp1 = EmployeeModel(id=1,name="Raja",age=37, department="MES")
+emp2 = EmployeeModel(id=2,name="Steve",age=47, department="engineering")
 
-def get_employee_or_404(emp_id: str) -> Employee:
+employees: list[EmployeeModel] = []
+
+employees.append(emp1)
+employees.append(emp2)
+
+def get_employee_or_404(emp_id: int) -> EmployeeModel:
     for emp in employees:
         if emp.id == emp_id:
             return emp
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Employee Id - {emp_id} not found")
 
-@app.post("/employees",status_code = status.HTTP_201_CREATED)
-def add_employee(employee: Employee):
+"""
+need check if there is a simple way like employees.orderBy(x => x.id)
+"""
+def get_new_emp_id() -> int:
+    max_id: int = 0
+
     for emp in employees:
-            if emp.id == employee.id:
-                 raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Employee Id - {employee.id} already exist")
-    employees.append(employee)
-    return employee
+        if emp.id > max_id:
+            max_id = emp.id
+
+    return max_id + 1
+
+### Create Employee
+@app.post("/employees",status_code = status.HTTP_201_CREATED)
+def add_employee(employee_base: EmployeeBase):
+    print(f"existing length - {len(employees)}")
+    new_id = get_new_emp_id()
+    employee_model = EmployeeModel(id=new_id,name=employee_base.name,age=employee_base.age,department=employee_base.department)
+    employees.append(employee_model)
+    return employee_model
 
 #path parameter
-# returns a specific employee
+### Get Employee by ID
 @app.get("/employees/{id}", status_code=status.HTTP_200_OK)
-def get_employee(id: str):
+def get_employee(id: int):
      return get_employee_or_404(id)
 
-#returs all employees
+### Get All Employees
 @app.get("/employees")
-def get_employees() -> list[Employee]:
+def get_employees() -> list[EmployeeModel]:
      return employees
+
+@app.put("/employees/{id}", status_code=status.HTTP_200_OK)
+def update_employee(id: int, updated_employee: EmployeeBase):
+    for index, emp in enumerate(employees):
+        if emp.id == id:
+            updated_employee_model = EmployeeModel(id=id,name=updated_employee.name,age=updated_employee.age,department=updated_employee.department)
+            employees[index] = updated_employee_model
+            return updated_employee_model
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Employee Id - {id} not found")
+
+"""
+i wanted to use the existing method and update somthing like this but it's not available
+def update_employee(id: str, updated_employee: Employee):
+    existing_emp = get_employee_or_404(id)
+    existing_emp.update(model_dump())
+"""
+
+@app.delete("/employees/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_employee(id: int):
+     existing_emp = get_employee_or_404(id)
+     employees.remove(existing_emp)
+        
 
 
         
